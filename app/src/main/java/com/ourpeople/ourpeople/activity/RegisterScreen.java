971a729cfed035a0e7deb9a1baa8c6a259cb61be
Hareesh Pammi <hareesh.pammi@C02QT217G8WP.local>
2016-09-10 21:51:13 +0530

    Added the Register screen changes

--- app/src/main/java/com/ourpeople/ourpeople/activity/RegisterScreen.java
+++ app/src/main/java/com/ourpeople/ourpeople/activity/RegisterScreen.java
@@ -1,8 +1,13 @@
 package com.ourpeople.ourpeople.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.ourpeople.ourpeople.R;
 
@@ -14,5 +19,39 @@ public class RegisterScreen extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen);
+
+        TextInputEditText emailInSignup = (TextInputEditText) findViewById(R.id.et_signupemail);
+        TextInputEditText passwordInSignup = ((TextInputEditText) findViewById(R.id.et_signuppassword));
+
+        emailInSignup.setOnFocusChangeListener((s,v) -> {
+            if (!v) {
+                if (!(Patterns.EMAIL_ADDRESS.matcher(emailInSignup.getText()).matches())) {
+                    Toast.makeText(RegisterScreen.this, "Invalid Email", Toast.LENGTH_SHORT).show();
+                    emailInSignup.setError("Invalid Email");
+                    emailInSignup.setText("");
+                }
+            }
+        });
+
+        passwordInSignup.setOnFocusChangeListener((s,v) -> {
+            if (!v) {
+                if (!(passwordInSignup.getText().length() < 6)) {
+                    Toast.makeText(RegisterScreen.this, "Password should contain minimum 6 characters", Toast.LENGTH_SHORT).show();
+                    passwordInSignup.setError("Password should contain minimum 6 characters");
+                    passwordInSignup.setText("");
+                }
+            }
+        });
+
+        Button register = ((Button) findViewById(R.id.btn_register));
+        register.setOnClickListener(s -> {
+            Intent homeScreenIntent = new Intent(RegisterScreen.this, HomeScreen.class);
+            RegisterScreen.this.startActivity(homeScreenIntent);
+        });
+
+        (findViewById(R.id.ll_logintext)).setOnClickListener(s -> {
+            Intent loginIntent = new Intent(RegisterScreen.this, SplashScreen.class);
+            RegisterScreen.this.startActivity(loginIntent);
+        });
     }
 }
--- app/src/main/java/com/ourpeople/ourpeople/activity/SplashScreen.java
+++ app/src/main/java/com/ourpeople/ourpeople/activity/SplashScreen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.ourpeople.ourpeople.R;
@@ -12,12 +13,11 @@ import com.ourpeople.ourpeople.R;
 /**
  * Created by hareesh.pammi on 29/07/16.
  */
-public class SplashScreen extends AppCompatActivity{
+public class SplashScreen extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-        //View loginView = findViewById(R.id.ll_login);
 
         (findViewById(R.id.ll_signuptext)).setOnClickListener(s -> {
             Intent signupIntent = new Intent(SplashScreen.this, RegisterScreen.class);
@@ -25,8 +25,8 @@ public class SplashScreen extends AppCompatActivity{
         });
 
         TextInputEditText emailText = (TextInputEditText) findViewById(R.id.email);
-        emailText.setOnFocusChangeListener((s,v) ->{
-            if(!v) {
+        emailText.setOnFocusChangeListener((s, v) -> {
+            if (!v) {
                 if (!(Patterns.EMAIL_ADDRESS.matcher(emailText.getText()).matches())) {
                     Toast.makeText(SplashScreen.this, "Invalid Email", Toast.LENGTH_SHORT).show();
                     emailText.setError("Invalid Email");
@@ -35,17 +35,21 @@ public class SplashScreen extends AppCompatActivity{
             }
         });
 
-        
-
         TextInputEditText password = ((TextInputEditText) findViewById(R.id.pwd));
-        password.setOnFocusChangeListener((s,v) -> {
-            if(!v) {
+        password.setOnFocusChangeListener((s, v) -> {
+            if (!v) {
                 if (!(password.getText().length() < 6)) {
                     Toast.makeText(SplashScreen.this, "Password should contain minimum 6 characters", Toast.LENGTH_SHORT).show();
                     password.setError("Password should contain minimum 6 characters");
                     password.setText("");
                 }
             }
+        });
+
+        Button loginBtn = ((Button) findViewById(R.id.btn_login));
+        loginBtn.setOnClickListener(s -> {
+            Intent homeScreenIntent = new Intent(SplashScreen.this, HomeScreen.class);
+            SplashScreen.this.startActivity(homeScreenIntent);
         });
 
     }
